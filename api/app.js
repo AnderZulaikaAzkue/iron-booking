639afea0223fd51ce27319734754482b17b00577
Ander Zulaika <anderzu85@gmail.com>
2023-04-17 12:03:17 +0200

    mongoose valitadion errors created

--- api/app.js
+++ api/app.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 const createError = require('http-errors');
 
 require('./config/db.config');
 const app = express();
 
+app.use(express.json());
 app.use(logger('dev'));
 
 const api = require('./config/routes.config');
@@ -16,12 +18,12 @@ app.use((req, rest, next) => {
   throw new Error ('ERROOORRR')
 })
 
-
-
 app.use((req, res, next) => next(createError(404, 'Route not found, bad luck')));
 
 app.use((error, req, res, next) => {
-  if (!error.status) {
+  if(error instanceof mongoose.Error.ValidationError){
+    error = createError(400, error);
+  } else if (!error.status) {
     error = createError(500, error);
   }
   console.error(error);
@@ -29,6 +31,16 @@ app.use((error, req, res, next) => {
   const data = {
     message: error.message,
   }
+
+  if (error.errors) {
+    const errors = Object.keys(error.errors)
+      .reduce((errors, errorKey) => {
+        errors[errorKey] = error.errors[errorKey].message;
+        return errors;
+      }, {});
+    data.errors = errors;
+  }
+
   res.status(error.status)
     .json(data)
 })
