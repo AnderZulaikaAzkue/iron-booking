994df85f40a5ab1dc24fd1b5e993063b47aee3ca
Ander Zulaika <anderzu85@gmail.com>
2023-04-17 17:22:25 +0200

    middleware findById

--- api/config/routes.config.js
+++ api/config/routes.config.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const hotel = require('../controllers/hotel.controllers');
+const hotelMid = require("../middlewares/hotel.mid");
 
 router.get("/hotels", hotel.list);
 router.post("/hotels", hotel.create);
-router.get("/hotels/:id", hotel.detail);
-router.patch("/hotels/:id", hotel.update);
-router.delete("/hotels/:id", hotel.delete)
+router.get("/hotels/:id", hotelMid.exists, hotel.detail);
+router.patch("/hotels/:id", hotelMid.exists, hotel.update);
+router.delete("/hotels/:id", hotelMid.exists, hotel.delete)
 
 module.exports = router;
 
---
+++ api/middlewares/hotel.mid.js
@@ -0,0 +1,17 @@
+const Hotel = require('../models/hotel.model');
+const createError = require('http-errors');
+
+module.exports.exists = (req, res, next) => {
+  const hotelId = req.params.projectId || req.params.id;
+  Hotel.findById(hotelId)
+  .populate("comments")
+  .then((hotel) => {
+    if (hotel) {
+      req.hotel = hotel;
+      next();
+    } else {
+      next(createError(404, 'Hotel not found'))
+    }
+  })
+  .catch(next)
+}
