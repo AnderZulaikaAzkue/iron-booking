12d5b599a15f23f1c9dea07b00f1b0ad8dd99387
Ander Zulaika <anderzu85@gmail.com>
2023-04-30 00:11:11 +0200

    room  detail styling

--- web/src/components/hotelItem/hotelItem.js
+++ web/src/components/hotelItem/hotelItem.js
@@ -4,20 +4,8 @@ import { Link } from 'react-router-dom';
  function HotelItem({hotel: { id, name, picture, city, rooms }}) {
   return (
     <div className="project-item card text-bg-dark">
-    <img src={picture} className="card-img" alt={name} />
-    <div className="card-img-overlay">
-      <div className="d-flex flex-column h-100">
-        <div className="d-flex align-items-baseline">
-          <h5 className="card-title px-3 py-1 fs-5 fw-lighter text-dark me-auto"><Link to={`/hotels/${id}`} className='text-dark link-underline-opacity-0'>{name}</Link></h5>
-          <a href={rooms} target='blank' className='text-dark fs-3 link-underline-opacity-0'><i className=''></i></a>
-        </div>
-        <div className="d-flex mt-auto align-items-center">
-          <div className='me-auto'>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+     <Link to={`/hotels/${id}`}> <img src={picture} className="card-img" alt={name} width={600}/> </Link>
+     </div> 
   )
 }
 export default HotelItem
--- web/src/components/roomDetail/roomDetail.js
+++ web/src/components/roomDetail/roomDetail.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import hotelsService from '../../services/hotels';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+import { Card, Col, Row, Button, Text } from "@nextui-org/react";
+import Footer from "../footer/footer";
 
 
- function RoomDetail() {
+function RoomDetail() {
   const { roomsId } = useParams();
   const navigate = useNavigate();
   const [rooms, setRoom] = useState();
@@ -28,15 +30,89 @@ import { useParams, useNavigate } from 'react-router-dom';
 
   return (
     <>
-      {!rooms? (<p><i className='fa fa-gear fa-spin'></i>Loading...</p>) : (
-        <>
-          <h1>{rooms?.name}</h1>
-          <h2> {rooms?.price} </h2>
-          <img src={rooms?.picture} alt="" />
-          {rooms?.roomNumbers.map(room => <div>{room.number}</div>)}
-         
-        </>
-      )} 
+      <div className="hotelDetailContainer">
+        {!rooms ? (<p><i className='fa fa-gear fa-spin'></i>Loading...</p>) : (
+          <>
+            <Card css={{ w: "100%", h: "400px" }}>
+              <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+                <Col>
+                  <Text size={18} weight="bold" transform="uppercase" color="#9E9E9E">
+                    {rooms?.name}
+                  </Text>
+                  <Text h3 color="white">
+                   Room price {rooms?.price} 
+                  </Text>
+                </Col>
+              </Card.Header>
+              <Card.Body css={{ p: 0 }}>
+                <Card.Image
+                  src={rooms?.picture}
+                  objectFit="cover"
+                  width="100%"
+                  height="100%"
+                  alt="Relaxing app background"
+                />
+              </Card.Body>
+              <Card.Footer
+                isBlurred
+                css={{
+                  position: "absolute",
+                  bgBlur: "#0f111466",
+                  borderTop: "$borderWeights$light solid $gray800",
+                  bottom: 0,
+                  zIndex: 1,
+                }}
+              >
+                <Row>
+                  <Col>
+                    <Row>
+                      <Col span={3}>
+                        <Link to="/hotels">
+                          <Card.Image
+                            src="https://t4.ftcdn.net/jpg/03/94/32/75/360_F_394327546_pFLFWqRXpA0zefbrQBMeCkr0epQBmeWS.jpg"
+                            css={{ bg: "black", br: "50%" }}
+                            height={40}
+                            width={40}
+                            alt="Breathing app icon"
+                          />
+                        </Link>
+                      </Col>
+                      <Col>
+
+                        <Text color="#d1d1d1" size={20}>
+                          {rooms?.description}
+                        </Text>
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col>
+                    <Row justify="flex-end">
+                      <Button
+                        flat
+                        auto
+                        rounded
+                        css={{ color: "#94f9f0", bg: "#94f9f026" }}
+                      >
+                        <Link to={{}} >
+                          <Text
+                            css={{ color: "inherit" }}
+                            size={16}
+                            weight="bold"
+                            transform="uppercase"
+                          >
+                            Book the room
+                          </Text>
+                        </Link>
+                      </Button>
+                    </Row>
+                  </Col>
+                </Row>
+              </Card.Footer>
+            </Card>
+          </>
+        )}
+      </div>
+      <Footer />
     </>
   )
 }
